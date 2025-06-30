faff675305c6035cec30c6032b75adf4fd696123
TanishqSingla <tanishqsingla00@gmail.com>
2025-06-30 16:37:10 +0530

    feat: add support for subject and sender

--- services/smtp.go
+++ services/smtp.go
@@ -1,14 +1,26 @@
 package services
 
 import (
+	"errors"
 	"net/smtp"
+	"strings"
 	"video-streaming-server/config"
 	"video-streaming-server/shared/logger"
 )
 
-func SendEmail(to []string, body string) error {
+//SendEmail is a wrapper over smtp.SendEmail.
+//
+//This function consumes information like host address, username and password from the config.
+//
+//Recipients is an array of string which is joined using comma (,) 
+//separator in the function, creating a comma separated string
+func SendEmail(recipients []string, sender string, subject string, body string) error {
+	if len(recipients) == 0 {
+		return errors.New("Recipients should not be emtpy")
+	}
+
 	user := config.AppConfig.SMTPUser
-	password := config.AppConfig.SMTPPAssword
+	password := config.AppConfig.SMTPPassword
 
 	host := config.AppConfig.SMTPHost
 	port := config.AppConfig.SMTPPort
@@ -16,7 +28,11 @@ func SendEmail(to []string, body string) error {
 
 	auth := smtp.PlainAuth("", user, password, host)
 
-	err := smtp.SendMail(addr, auth, user, to, []byte(body))
+	msg := "From: " + sender + "\r\n" +
+	"To: " + strings.Join(recipients, ",") + "\r\n" +
+	"Subject: " + subject + "\r\n" + body
+
+	err := smtp.SendMail(addr, auth, user, recipients, []byte(msg))
 
 	if err != nil {
 		logger.Log.Error(err.Error())
