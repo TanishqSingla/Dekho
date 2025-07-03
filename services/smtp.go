a3a1a91536a2154a5e99cf61f24f768a0ffab87e
TanishqSingla <tanishqsingla00@gmail.com>
2025-07-03 13:19:16 +0530

    feat: add support for html messages

--- services/smtp.go
+++ services/smtp.go
@@ -8,14 +8,22 @@ import (
 	"video-streaming-server/shared/logger"
 )
 
-//SendEmail is a wrapper over smtp.SendEmail.
+type message struct {
+	sender     string
+	recipients []string
+	isHTML     bool
+	subject    string
+	body       string
+}
+
+// SendEmail is a wrapper over smtp.SendEmail.
 //
-//This function consumes information like host address, username and password from the config.
+// This function consumes information like host address, username and password from the config.
 //
-//Recipients is an array of string which is joined using comma (,) 
-//separator in the function, creating a comma separated string
-func SendEmail(recipients []string, sender string, subject string, body string) error {
-	if len(recipients) == 0 {
+// Recipients is an array of string which is joined using comma (,)
+// separator in the function, creating a comma separated string
+func SendEmail(msg *message) error {
+	if len(msg.recipients) == 0 {
 		return errors.New("Recipients should not be emtpy")
 	}
 
@@ -28,11 +36,17 @@ func SendEmail(recipients []string, sender string, subject string, body string) 
 
 	auth := smtp.PlainAuth("", user, password, host)
 
-	msg := "From: " + sender + "\r\n" +
-	"To: " + strings.Join(recipients, ",") + "\r\n" +
-	"Subject: " + subject + "\r\n" + body
+	message := "From: " + msg.sender + "\r\n" +
+		"To: " + strings.Join(msg.recipients, ",") + "\r\n" +
+		"Subject: " + msg.subject + "\r\n"
 
-	err := smtp.SendMail(addr, auth, user, recipients, []byte(msg))
+	if msg.isHTML {
+		message += "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	}
+
+	message += msg.body
+
+	err := smtp.SendMail(addr, auth, user, msg.recipients, []byte(message))
 
 	if err != nil {
 		logger.Log.Error(err.Error())
